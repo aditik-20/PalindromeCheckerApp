5879f3a5b4fd81167a67cd7a43e4122d124f91af
Aditi Krishna <ak.krishnaaditi@gmail.com>
2026-02-13 10:51:46 +0530

    Application Entry and Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+    /*
+    Application entry Point
+
+    This is the first method executed by the JVM
+    when the program starts
+
+    @author Developer
+    @version 1.0
+     */
+
+        public static  void main(String []args){
+            System.out.println("Welcome to the Palindrom Checker");
+            System.out.println("Version 1.0");
+            System.out.println("System initialised succesfully");
+        }
+}

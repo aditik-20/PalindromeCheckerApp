1b5bb5c6793d74c565d24b700aa63c2142def8b1
aditik-20 <ak.krishnaaditi@gmail.com>
2026-02-20 09:57:17 +0530

    UC4 Palindrom Check by Character Array

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,17 +5,16 @@ public class PalindromeCheckerApp {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text : ");
         String input = sc.nextLine();
-        StringBuilder sb = new StringBuilder();
-        for(int i = input.length() - 1; i >= 0 ; i--){
-            sb.append(input.charAt(i));
-        }
-        String reverse = sb.toString();
-
-            if(input.equals(reverse)){
-                System.out.println("Is Palindrome ? true "  );
-            }else{
-                System.out.println("Is Palindrome ? false "  );
+        char arr[] = input.toCharArray();
+        int n = input.length();
+        boolean status = true;
+        for(int i = 0 ; i < n/2 ; i++){
+            if(arr[i] != arr[n - i - 1]){
+                status = false;
             }
+        }
+
+            System.out.println("Is Palindrom: " + status);
 
 
 

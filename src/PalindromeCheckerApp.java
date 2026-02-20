2bdc6149f12138f575efac87b2d05a336573f6da
aditik-20 <ak.krishnaaditi@gmail.com>
2026-02-20 10:05:10 +0530

    UC5 StackBased Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,20 +5,21 @@ public class PalindromeCheckerApp {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text : ");
         String input = sc.nextLine();
-        char arr[] = input.toCharArray();
-        int n = input.length();
-        boolean status = true;
-        for(int i = 0 ; i < n/2 ; i++){
-            if(arr[i] != arr[n - i - 1]){
-                status = false;
-            }
+       Stack<Character> st = new Stack<>();
+       for(int i = 0 ; i < input.length();i++){
+           st.push(input.charAt(i));
+       }
+//        System.out.println(st);
+        StringBuilder sb = new StringBuilder();
+        while(st.size()!=0){
+            sb.append((st.pop()));
         }
-
-            System.out.println("Is Palindrom: " + status);
-
-
-
-
+        String reverse = sb.toString();
+        if(input.equals(reverse)){
+            System.out.println("Is Palindrome? true");
+        }else{
+            System.out.println("Is Palindrome? false");
+        }
     }
 }
 

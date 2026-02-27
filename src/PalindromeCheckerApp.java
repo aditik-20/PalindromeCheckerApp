f9ef386a6ad4ce8c0142bae677bde0ff41629b60
aditik-20 <ak.krishnaaditi@gmail.com>
2026-02-27 10:02:17 +0530

    UC9RecursivePalindromeChecker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,73 +1,11 @@
 import java.util.*;
 
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
 
-    static Node createList(String str) {
-        Node head = null, tail = null;
-
-        for (char ch : str.toCharArray()) {
-            Node newNode = new Node(ch);
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-
-    static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
+    static boolean palidrome(String s , int start , int end){
+        if(start >= end) return  true;
+        if(s.charAt(start)!=s.charAt(end)) return false;
+        return palidrome(s,start+1,end -1);
     }
 
     public static void main(String[] args) {
@@ -75,8 +13,8 @@ public class PalindromeCheckerApp {
         System.out.print("Input text : ");
         String input = sc.nextLine();
 
-        Node head = createList(input);
-        System.out.println("Is Palindrom? :"+isPalindrome(head));
+
+        System.out.println("Is Palindrom? :"+ palidrome(input,0,input.length()-1));
 
         sc.close();
     }
